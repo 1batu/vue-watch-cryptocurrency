60f367036f00eba4faad6b42d04dfeee42223c25
batuhan <haymana.batuhan@gmail.com>
2021-02-07 21:18:56 +0300

    boostrap add

--- watch-cripto/src/App.vue
+++ watch-cripto/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <b-button variant="success">Button</b-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
