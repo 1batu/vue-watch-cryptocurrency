5dc14b54f22b07310bf6d99ec815c5b66db54a8c
batuhan <haymana.batuhan@gmail.com>
2021-02-08 00:17:26 +0300

    api image search

--- watch-cripto/src/components/Home.vue
+++ watch-cripto/src/components/Home.vue
@@ -18,23 +18,48 @@
     </div>
 </template>
 <script>
+import axios from 'axios';
 export default {
     data() {
         return {
             coins:[
-                {id:'1',name:'Ethereum (ETH)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://cdn.pixabay.com/photo/2018/09/07/09/10/ethereum-3660218_960_720.jpg)'},
-                {id:'2',name:'Litecoin (LTC)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://lh3.googleusercontent.com/proxy/iitni2U-zacNlq3Qa8N2-ml4NlL3yyRjS2rKZKuAyj0NbcWk2B1Ch09XsyOMbg7OPhz5UI3tYPSlRFZJJPww-CfptBj2AlWd3BsnWF1bTi4ms2Yn0l6c)'},
-                {id:'3',name:'Cardano (ADA)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://www.coinkolik.com/wp-content/uploads/2021/02/cardano-ada.jpg)'},
-                {id:'4',name:'Polkadot (DOT)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://i2.wp.com/kriptokoin.com/wp-content/uploads/2020/08/polkadot-sale-1200x675-1.jpg?fit=770%2C433&ssl=1)'},
-                {id:'5',name:'Bitcoin Cash (BCH)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://www.webtekno.com/images/editor/default/0002/56/fd60c6c5a3a4cb487677fcbb8d884f5fa883ec94.jpeg)'},
-                {id:'6',name:'Stellar (XLM)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://stormgain.com/sites/default/files/news/Stellar%20Currency.jpg)'},
-                {id:'7',name:'Chainlink',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://i1.wp.com/kriptokoin.com/wp-content/uploads/2020/08/Chainlink.jpg?fit=1200%2C780&ssl=1)'},
-                {id:'8',name:'Binance Coin (BNB)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://www.coinkolik.com/wp-content/uploads/2019/09/Binance-Coin-BNB.jpg)'},
-                {id:'9',name:'Tether (USDT)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://media.cointral.com/wp-content/uploads/sites/2/2019/09/08014322/buy_tether.jpg)'},
-                {id:'10',name:'Monero (XMR)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://uzmancoin.com/wp-content/uploads/2020/10/monero-xmr.jpg)'},
-                {id:'11',name:'Dogecoin (DOGE)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://iasbh.tmgrup.com.tr/fcbc7f/650/344/0/2/1280/673?u=https://isbh.tmgrup.com.tr/sbh/2021/01/29/dogecoin-nedir-ve-nasil-alinir-dogecoin-kac-tl-oldu-1611901812473.jpg)'},
-                {id:'12',name:'Tether (USDT)',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://i.ytimg.com/vi/9j5KqATrr3M/maxresdefault.jpg)'}
-            ]
+                {id:'1',name:'Ethereum (ETH)',search:'ethereum',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://cdn.pixabay.com/photo/2018/09/07/09/10/ethereum-3660218_960_720.jpg)'},
+                {id:'2',name:'Litecoin (LTC)',search:'litecoin',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://lh3.googleusercontent.com/proxy/iitni2U-zacNlq3Qa8N2-ml4NlL3yyRjS2rKZKuAyj0NbcWk2B1Ch09XsyOMbg7OPhz5UI3tYPSlRFZJJPww-CfptBj2AlWd3BsnWF1bTi4ms2Yn0l6c)'},
+                {id:'3',name:'Cardano (ADA)',search:'cordano',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://www.coinkolik.com/wp-content/uploads/2021/02/cardano-ada.jpg)'},
+                {id:'4',name:'Polkadot (DOT)',search:'polkadot',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://i2.wp.com/kriptokoin.com/wp-content/uploads/2020/08/polkadot-sale-1200x675-1.jpg?fit=770%2C433&ssl=1)'},
+                {id:'5',name:'Bitcoin Cash (BCH)',search:'bitcoin',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://www.webtekno.com/images/editor/default/0002/56/fd60c6c5a3a4cb487677fcbb8d884f5fa883ec94.jpeg)'},
+                {id:'6',name:'Stellar (XLM)',search:'stellar',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://stormgain.com/sites/default/files/news/Stellar%20Currency.jpg)'},
+                {id:'7',name:'Chainlink',search:'chainlink',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://i1.wp.com/kriptokoin.com/wp-content/uploads/2020/08/Chainlink.jpg?fit=1200%2C780&ssl=1)'},
+                {id:'8',name:'Binance Coin (BNB)',search:'binance-coin',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://www.coinkolik.com/wp-content/uploads/2019/09/Binance-Coin-BNB.jpg)'},
+                {id:'9',name:'Tether (USDT)',search:'tether',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://media.cointral.com/wp-content/uploads/sites/2/2019/09/08014322/buy_tether.jpg)'},
+                {id:'10',name:'Monero (XMR)',search:'monero',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://uzmancoin.com/wp-content/uploads/2020/10/monero-xmr.jpg)'},
+                {id:'11',name:'Dogecoin (DOGE)',search:'dogecoin',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://iasbh.tmgrup.com.tr/fcbc7f/650/344/0/2/1280/673?u=https://isbh.tmgrup.com.tr/sbh/2021/01/29/dogecoin-nedir-ve-nasil-alinir-dogecoin-kac-tl-oldu-1611901812473.jpg)'},
+                {id:'12',name:'Tether (USDT)',search:'tether',style:'background-color:rgba(54, 25, 25, .00004);background-image: url(https://i.ytimg.com/vi/9j5KqATrr3M/maxresdefault.jpg)'}
+            ],
+            apikey:'12900266-1f5815a23ec1f14f4c6b18e1e'
+        }
+    },
+    mounted() {
+        this.getImage()
+    },
+    methods: {
+        getImage()
+        { 
+            for (const element of this.coins) {
+                //console.log(element.name)
+                let url = 'https://pixabay.com/api/?key='+this.apikey+'&q='+element.search+'&image_type=photo&pretty=true&min_width=200';
+                axios.get(url).then(response=>{
+                    if(response.data.hits.length > 0)
+                    {
+                        //this.coins.style = 'background-color:rgba(54, 25, 25, .00004);'+'background-image: url('+response.data.hits[0].largeImageURL+')';
+                        console.log(response.data.hits[0].largeImageURL);
+                    }
+                    else
+                    {
+                        console.log('Error')
+                    }
+                });
+            }
         }
     },
 }
